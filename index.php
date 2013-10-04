5b8604bb9cc7e5b42c271e54a38c1fd7561659c5
kamaldeep <kdaryan308@gmail.com>
2013-10-04 18:13:41 +0530

    TEST
    DESC

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_REQUEST['submit1'])){
+echo $_REQUEST['fname'];
+echo $_REQUEST['lname'];
+echo $_REQUEST['email'];
+echo $_REQUEST['pass'];
+}
+?>
+
+<html>
+<head>
+	<title>Forms</title>
+</head>
+<body>
+	<form action="#" method="post">
+		First Name: <input type="text" name="fname" placeholder="Enter first name"><br><br>
+		Last Name: <input type="text" name="lname" placeholder="Enter last name"><br><br>
+		E-mail:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" name="email" placeholder="abx@xyz.com"><br><br>
+		Password: &nbsp;&nbsp;<input type="text" name="pass" placeholder="Password"><br><br>
+		<input type="Submit" name="submit">
+		<input type="reset" name="reset">
+	</form>
+
+</body>
+</html>
